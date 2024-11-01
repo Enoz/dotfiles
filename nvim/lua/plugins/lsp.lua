f391574b184a0ba401854b49f34bda3789b025ac
Enoz <adudek013@gmail.com>
2024-11-01 10:04:22 -0500

    Remove some comments

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,8 +2,6 @@ return {
     {
         'neovim/nvim-lspconfig',
         config = function()
-            -- Add cmp_nvim_lsp capabilities settings to lspconfig
-            -- This should be executed before you configure any language server
             local lspconfig_defaults = require('lspconfig').util.default_config
             lspconfig_defaults.capabilities = vim.tbl_deep_extend(
                 'force',
@@ -11,8 +9,6 @@ return {
                 require('cmp_nvim_lsp').default_capabilities()
             )
 
-            -- This is where you enable features that only work
-            -- if there is a language server active in the file
             vim.api.nvim_create_autocmd('LspAttach', {
                 desc = 'LSP actions',
                 callback = function(event)
