7f395ea6201c5bfa2c81334b7224f58ab0d2a26a
Enoz <adudek013@gmail.com>
2024-11-01 15:13:58 -0500

    Treesitter

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,37 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {},
+                highlight = {
+                    enable = true,
+                },
+                indent = {
+                    enable = true,
+                },
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = "@class.inner",
+                            -- Add more mappings as needed
+                        },
+                    },
+                },
+            })
+        end,
+    },
+}
