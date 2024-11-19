2f9fd8e2699b5053c6e641933032e703e7590133
Enoz <adudek013@gmail.com>
2024-11-19 11:01:59 -0500

    Treesitter auto install

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
         },
         config = function()
             require("nvim-treesitter.configs").setup({
-                ensure_installed = { "c", "cpp", "python" }, -- Specify the languages you want
+                auto_install = true,
                 highlight = {
                     enable = true,
                 },
