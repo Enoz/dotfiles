ecf05b1c87f3a4808f959c1d63bc1729b520e4c8
Enoz <adudek013@gmail.com>
2024-11-25 13:17:36 -0600

    Remove close on select

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -10,12 +10,6 @@ return {
         -- disable netrw
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
-        require('nvim-tree').setup({
-            actions = {
-                open_file = {
-                    quit_on_open = true,
-                },
-            },
-        })
+        require('nvim-tree').setup()
     end
 }
