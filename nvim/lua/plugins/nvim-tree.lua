44b5f7322d955ff182d55ed3b980bbf4bfd3c05d
Enoz <adudek013@gmail.com>
2024-11-25 16:28:44 -0600

    better nvim-tree keybinds

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -4,12 +4,19 @@ return {
         'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     },
     keys = {
-        { '\\', ':NvimTreeToggle <CR>', desc = 'NvimTree reveal', silent = true },
+        { '\\', ':NvimTreeFocus <CR>', desc = 'NvimTree reveal', silent = true },
     },
     config = function()
         -- disable netrw
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
-        require('nvim-tree').setup()
+        require('nvim-tree').setup({
+            on_attach = function(bufnr)
+                local api = require('nvim-tree.api')
+                api.config.mappings.default_on_attach(bufnr)
+
+                vim.keymap.set('n', '\\', api.tree.close, { buffer = bufnr, noremap = true, silent = true })
+            end
+        })
     end
 }
