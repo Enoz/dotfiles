e606d1f7a2a24d9518eab26c490edca16567932c
Enoz <adudek013@gmail.com>
2024-11-25 13:07:30 -0600

    Replace neotree with nvimtree

---
+++ nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,21 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+        'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    },
+    keys = {
+        { '\\', ':NvimTreeToggle <CR>', desc = 'NvimTree reveal', silent = true },
+    },
+    config = function()
+        -- disable netrw
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+        require('nvim-tree').setup({
+            actions = {
+                open_file = {
+                    quit_on_open = true,
+                },
+            },
+        })
+    end
+}
