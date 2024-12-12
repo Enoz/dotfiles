064fee65fe3ce8d1c065e0f74032a4913bae903b
Enoz <adudek013@gmail.com>
2024-12-12 07:49:44 -0600

    Organization, LSP References

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,14 +7,14 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		local telescope = require("telescope")
 		local builtin = require("telescope.builtin")
-
-		telescope.load_extension("fzf")
+		require("telescope").setup()
+		require("telescope").load_extension("fzf")
 
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+		vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "Telescope LSP References" })
 	end,
 }
