99a71d32280e04ede8362530740df25f2892af8a
Enoz <adudek013@gmail.com>
2024-12-16 11:03:15 -0600

    Lualine gitblame

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,7 +1,18 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+	dependencies = { "nvim-tree/nvim-web-devicons", "f-person/git-blame.nvim" },
 	config = function()
-		require("lualine").setup()
+		-- Remove virtual text from gitblame, only want it in lualine
+		local git_blame = require("gitblame")
+		git_blame.setup({
+			display_virtual_text = 0,
+		})
+		require("lualine").setup({
+			sections = {
+				lualine_c = {
+					{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
+				},
+			},
+		})
 	end,
 }
