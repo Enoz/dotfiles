9d424ed95f10d2aa27b920d8883333ca1316da2a
Enoz <adudek013@gmail.com>
2024-12-16 11:19:06 -0600

    Add gitblame keymaps

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,11 +2,13 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons", "f-person/git-blame.nvim" },
 	config = function()
-		-- Remove virtual text from gitblame, only want it in lualine
 		local git_blame = require("gitblame")
+
 		git_blame.setup({
 			display_virtual_text = 0,
 		})
+		vim.keymap.set("n", "<leader>go", "<cmd>:GitBlameOpenFileURL<CR>", { desc = "Git Open File" })
+		vim.keymap.set("n", "<leader>gc", "<cmd>:GitBlameCopyFileURL<CR>", { desc = "Git Copy File" })
 		require("lualine").setup({
 			sections = {
 				lualine_c = {
