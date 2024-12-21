df62255f897fa943f23f68bb70206af8f9df54c2
Enoz <adudek013@gmail.com>
2024-12-21 10:55:14 -0600

    Update blink.cmp

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,5 +1,6 @@
 return {
 	"saghen/blink.cmp",
+
 	lazy = false,
 	dependencies = "rafamadriz/friendly-snippets",
 	version = "v0.*",
@@ -8,22 +9,27 @@ return {
 			return vim.bo.buftype ~= "prompt" and vim.b.completion ~= false and vim.bo.filetype ~= "DressingInput"
 		end,
 		appearance = {
-			nerd_font_variant = "normal",
+			nerd_font_variant = "mono",
 		},
+
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
-			per_filetype = {
-				NvimTree = {},
-			},
 		},
 		completion = {
 			documentation = {
 				auto_show = true,
 				auto_show_delay_ms = 0,
 			},
-		},
-		draw = {
-			treesitter = { "lsp" },
+			menu = {
+				draw = {
+					columns = {
+						{ "kind_icon" },
+						{ "label", "label_description", gap = 1 },
+						{ "kind", gap = 1 },
+						{ "source_name" },
+					},
+				},
+			},
 		},
 
 		signature = { enabled = true },
