bfe3b4bf237c1b5020fd6d2482cebe769addafa1
Enoz <adudek013@gmail.com>
2025-02-16 10:16:32 -0600

    Floating nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -9,8 +9,34 @@ return {
 		-- "\" will always focus into nvim-tree
 		-- BUT, if you are already focused into nvim-tree, when "\" will instead close its
 		vim.keymap.set("n", "\\", "<cmd>:NvimTreeFindFile<CR>", { desc = "Open Nvim Tree", silent = true })
+
+		local HEIGHT_RATIO = 0.8 -- You can change this
+		local WIDTH_RATIO = 0.5 -- You can change this too
 		require("nvim-tree").setup({
-			view = { width = { min = 30 } },
+			view = {
+				float = {
+					enable = true,
+					-- https://github.com/MarioCarrion/videos/blob/269956e913b76e6bb4ed790e4b5d25255cb1db4f/2023/01/nvim/lua/plugins/nvim-tree.lua
+					open_win_config = function()
+						local screen_w = vim.opt.columns:get()
+						local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+						local window_w = screen_w * WIDTH_RATIO
+						local window_h = screen_h * HEIGHT_RATIO
+						local window_w_int = math.floor(window_w)
+						local window_h_int = math.floor(window_h)
+						local center_x = (screen_w - window_w) / 2
+						local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+						return {
+							border = "rounded",
+							relative = "editor",
+							row = center_y,
+							col = center_x,
+							width = window_w_int,
+							height = window_h_int,
+						}
+					end,
+				},
+			},
 			on_attach = function(bufnr)
 				local api = require("nvim-tree.api")
 				api.config.mappings.default_on_attach(bufnr)
