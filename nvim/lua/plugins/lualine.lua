fade8791cad13b1ac798c5d46c2d7dd1bae737ec
Enoz <adudek013@gmail.com>
2025-02-17 16:01:46 -0600

    Add filename to lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,13 +1,19 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons", "f-person/git-blame.nvim" },
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		{
+			"f-person/git-blame.nvim",
+			event = "VeryLazy",
+			opts = {
+				display_virtual_text = false,
+				gitblame_delay = 1,
+			},
+		},
+	},
 	config = function()
 		local git_blame = require("gitblame")
 
-		git_blame.setup({
-			display_virtual_text = 0,
-            gitblame_delay = 1
-		})
 		vim.keymap.set("n", "<leader>lo", "<cmd>:GitBlameOpenFileURL<CR>", { desc = "Git Open File" })
 		vim.keymap.set("n", "<leader>lc", "<cmd>:GitBlameCopyFileURL<CR>", { desc = "Git Copy File" })
 		vim.keymap.set("v", "<leader>lo", "<cmd>'<,'>:GitBlameOpenFileURL<CR>", { desc = "Git Open File" })
@@ -15,7 +21,8 @@ return {
 
 		require("lualine").setup({
 			sections = {
-				lualine_c = {
+				lualine_c = { "filename" },
+				lualine_x = {
 					{ git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
 				},
 			},
