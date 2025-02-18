19a763c30cbb752baa78c446207647520f31a353
Enoz <adudek013@gmail.com>
2025-02-18 09:05:35 -0600

    Lualine path

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -4,6 +4,12 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		require("lualine").setup({})
+		require("lualine").setup({
+			sections = {
+				lualine_c = {
+					{ "filename", path = 1 },
+				},
+			},
+		})
 	end,
 }
