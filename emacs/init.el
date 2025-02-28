e359f717ea0d8564b546eafc51d4a27355ca11cf
Enoz <adudek013@gmail.com>
2025-02-28 16:07:59 -0600

    Vertico

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,5 @@
+; Look into vertico, consult, orderless, emark, and marginalia
+
 (setq inhibit-startup-message t)
 
 (scroll-bar-mode -1)
@@ -30,9 +32,9 @@
   :config
   (evil-mode))
 
-(use-package ivy
+(use-package vertico
   :config
-  (ivy-mode))
+  (vertico-mode))
 
 
 (load-theme `tango-dark)
@@ -43,7 +45,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil ivy no-littering)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
