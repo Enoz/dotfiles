d4a0c70a36f1858f1b3ce9ae05bff8cd6d5984d8
Enoz <adudek013@gmail.com>
2025-03-01 14:53:22 -0600

    Update theme

--- emacs/init.el
+++ emacs/init.el
@@ -90,7 +90,7 @@
   :config
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-dark+ t))
+  (load-theme 'doom-acario-dark t))
 
 (use-package doom-modeline
   :ensure t
