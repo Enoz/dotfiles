8aff1dcad8428e457905ccc9c0ba2448462baf2e
Enoz <adudek013@gmail.com>
2025-03-19 17:35:09 -0500

    Add brighter highlight color

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,6 +1,13 @@
 return {
 	"EdenEast/nightfox.nvim",
 	config = function()
+		require("nightfox").setup({
+			groups = {
+				all = {
+					Visual = { bg = "#373737" },
+				},
+			},
+		})
 		vim.cmd("colorscheme carbonfox")
 	end,
 }
