ea79e36c44ef29fc109a4f20d39d3b56a0f25789
Enoz <adudek013@gmail.com>
2025-03-19 08:49:56 -0500

    Update blink config

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,67 +1,3 @@
-local keymap = {
-	preset = "none",
-
-	["<C-n>"] = {},
-	["<C-p>"] = {},
-
-	["<C-CR>"] = { "select_and_accept" },
-
-	["<C-j>"] = { "select_next", "fallback" },
-	["<C-k>"] = { "select_prev", "fallback" },
-
-	["<C-Space>"] = { "show" },
-	["<C-e>"] = { "hide" },
-
-	["<Tab>"] = { "snippet_forward", "fallback" },
-	["<S-Tab>"] = { "snippet_backward", "fallback" },
-
-	["<C-1>"] = {
-		function(cmp)
-			cmp.accept({ index = 1 })
-		end,
-	},
-	["<C-2>"] = {
-		function(cmp)
-			cmp.accept({ index = 2 })
-		end,
-	},
-	["<C-3>"] = {
-		function(cmp)
-			cmp.accept({ index = 3 })
-		end,
-	},
-	["<C-4>"] = {
-		function(cmp)
-			cmp.accept({ index = 4 })
-		end,
-	},
-	["<C-5>"] = {
-		function(cmp)
-			cmp.accept({ index = 5 })
-		end,
-	},
-	["<C-6>"] = {
-		function(cmp)
-			cmp.accept({ index = 6 })
-		end,
-	},
-	["<C-7>"] = {
-		function(cmp)
-			cmp.accept({ index = 7 })
-		end,
-	},
-	["<C-8>"] = {
-		function(cmp)
-			cmp.accept({ index = 8 })
-		end,
-	},
-	["<C-9>"] = {
-		function(cmp)
-			cmp.accept({ index = 9 })
-		end,
-	},
-}
-
 return {
 	"saghen/blink.cmp",
 	lazy = false,
@@ -71,23 +7,80 @@ return {
 		appearance = {
 			nerd_font_variant = "mono",
 		},
-		keymap = keymap,
+		keymap = {
+			preset = "none",
+
+			["<C-n>"] = {},
+			["<C-p>"] = {},
+
+			["<C-CR>"] = { "select_and_accept" },
+
+			["<C-j>"] = { "select_next", "fallback" },
+			["<C-k>"] = { "select_prev", "fallback" },
+
+			["<C-Space>"] = { "show" },
+			["<C-e>"] = { "hide" },
+
+			["<Tab>"] = { "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+			["<C-1>"] = {
+				function(cmp)
+					cmp.accept({ index = 1 })
+				end,
+			},
+			["<C-2>"] = {
+				function(cmp)
+					cmp.accept({ index = 2 })
+				end,
+			},
+			["<C-3>"] = {
+				function(cmp)
+					cmp.accept({ index = 3 })
+				end,
+			},
+			["<C-4>"] = {
+				function(cmp)
+					cmp.accept({ index = 4 })
+				end,
+			},
+			["<C-5>"] = {
+				function(cmp)
+					cmp.accept({ index = 5 })
+				end,
+			},
+			["<C-6>"] = {
+				function(cmp)
+					cmp.accept({ index = 6 })
+				end,
+			},
+			["<C-7>"] = {
+				function(cmp)
+					cmp.accept({ index = 7 })
+				end,
+			},
+			["<C-8>"] = {
+				function(cmp)
+					cmp.accept({ index = 8 })
+				end,
+			},
+			["<C-9>"] = {
+				function(cmp)
+					cmp.accept({ index = 9 })
+				end,
+			},
+		},
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
 		},
 		cmdline = {
-            keymap = keymap,
+			keymap = {
+				preset = "cmdline",
+			},
 			enabled = true,
 			completion = {
 				menu = {
 					auto_show = true,
-					draw = {
-						columns = {
-							{ "item_idx", "kind_icon", gap = 1 },
-							{ "label", "label_description", gap = 1 },
-							{ "source_name", gap = 1 },
-						},
-					},
 				},
 			},
 		},
@@ -125,5 +118,4 @@ return {
 
 		signature = { enabled = true },
 	},
-	opts_extend = { "sources.default" },
 }
