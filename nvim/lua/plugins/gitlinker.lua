7e44c86db3a2267bb0b1b70658fbe366df95391c
Enoz <adudek013@gmail.com>
2025-03-19 11:32:01 -0500

    Add C1

--- nvim/lua/plugins/gitlinker.lua
+++ nvim/lua/plugins/gitlinker.lua
@@ -1,7 +1,12 @@
 return {
 	"ruifm/gitlinker.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
-	opts = {
-		mappings = "<leader>g",
-	},
+	setup = function()
+		require("gitlinker").setup({
+			callbacks = {
+				["github.cloud.capitalone.com"] = require("gitlinker.hosts").get_github_type_url,
+			},
+			mappings = "<leader>g",
+		})
+	end,
 }
