c955c96447cb28d5f34e79f1c9be66bbe41ae7d7
Enoz <adudek013@gmail.com>
2025-03-19 12:06:41 -0500

    Fix range open

--- nvim/lua/plugins/git-blame.lua
+++ nvim/lua/plugins/git-blame.lua
@@ -5,7 +5,9 @@ return {
 		enabled = false,
 	},
 	keys = {
-		{ "<leader>gy", "<cmd>GitBlameCopyFileURL<CR>", mode = { "n", "v", desc = "Copy Git Link" } },
-		{ "<leader>gY", "<cmd>GitBlameOpenFileURL<CR>", mode = { "n", "v", desc = "Open Git Link" } },
+		{ "<leader>gy", "<cmd>GitBlameCopyFileURL<CR>", mode = { "n", desc = "Copy Git Link" } },
+		{ "<leader>gY", "<cmd>GitBlameOpenFileURL<CR>", mode = { "n", desc = "Open Git Link" } },
+		{ "<leader>gy", ":GitBlameCopyFileURL<CR>", mode = { "v", desc = "Copy Git Link" } },
+		{ "<leader>gY", ":GitBlameOpenFileURL<CR>", mode = { "v", desc = "Open Git Link" } },
 	},
 }
