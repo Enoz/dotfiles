a5bb68fedd072a37725bd12533ca5635187363ae
Enoz <adudek013@gmail.com>
2025-05-02 07:37:41 -0500

    Treesitter incremental

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -14,6 +14,15 @@ return {
 				indent = {
 					enable = true,
 				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<CR>",
+						node_incremental = "<CR>",
+						scope_incremental = false,
+						node_decremental = "<BS>",
+					},
+				},
 				textobjects = {
 					select = {
 						enable = true,
