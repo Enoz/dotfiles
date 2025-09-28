c98a9b5a057f91af3faedd3198ff30c939c28ce1
Enoz <enoz@enoz.dev>
2025-09-27 20:21:00 -0500

    More wezterm changes

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,25 +1,56 @@
--- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+local act = wezterm.action
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 
--- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
 config.initial_rows = 28
 
--- or, changing the font size and color scheme.
-config.font_size = 14
-config.color_scheme = 'carbonfox'
+config.font_size = 12
+config.color_scheme = "carbonfox"
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
 
 config.keys = {
-    {
-        key = 'Backspace',
-        mods = 'CTRL|SHIFT',
-        action = wezterm.action.CloseCurrentTab { confirm = false },
-    },
+	-- Close Window
+	{
+		key = "Backspace",
+		mods = "CTRL|SHIFT",
+		action = act.CloseCurrentPane({ confirm = false }),
+	},
+	-- Split Pane
+	{
+		key = "n",
+		mods = "CTRL|SHIFT",
+		action = act.ActivateKeyTable({
+			name = "split_pane",
+			timeout_milliseconds = 1000,
+		}),
+	},
+	-- Switch Pane
+	{ key = "h", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Right") },
+	-- Fullscreen
+	{ key = "F11", action = act.ToggleFullScreen },
+}
+
+config.key_tables = {
+	-- Split Pane
+	split_pane = {
+		{ key = "h", mods = "CTRL|SHIFT", action = act.SplitPane({ direction = "Left" }) },
+		{ key = "j", mods = "CTRL|SHIFT", action = act.SplitPane({ direction = "Down" }) },
+		{ key = "k", mods = "CTRL|SHIFT", action = act.SplitPane({ direction = "Up" }) },
+		{ key = "l", mods = "CTRL|SHIFT", action = act.SplitPane({ direction = "Right" }) },
+	},
 }
 
 -- Finally, return the configuration to wezterm:
