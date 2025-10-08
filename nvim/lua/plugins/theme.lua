d37d4ef1f0befb8eca251694c93488c1cfd25752
Enoz <enoz@enoz.dev>
2025-10-08 09:00:12 -0500

    Re-Add onedark

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,11 +1,13 @@
 return {
-	"navarasu/onedark.nvim",
-	priority = 1000, -- make sure to load this before all the other start plugins
+	"EdenEast/nightfox.nvim",
 	config = function()
-		require("onedark").setup({
-			style = "deep",
+		require("nightfox").setup({
+			groups = {
+				all = {
+					Visual = { bg = "#40313b" },
+				},
+			},
 		})
-		-- Enable theme
-		require("onedark").load()
+		vim.cmd("colorscheme carbonfox")
 	end,
 }
