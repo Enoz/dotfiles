2112f91be88a9b53a6b654c7e5ed91706e4c0af1
Enoz <enoz@enoz.dev>
2025-10-31 13:13:21 -0500

    wsl on windows

--- .wezterm.lua
+++ .wezterm.lua
@@ -87,7 +87,7 @@ config.key_tables = {
 -- Windows Powershell Config
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	config.default_prog = { "powershell.exe", "-NoLogo" }
+	config.default_prog = { "wsl.exe", "-d", "archlinux" }
 end
 
 return config
