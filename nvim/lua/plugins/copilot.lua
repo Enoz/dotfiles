61d848cb6247d1183613bd59f0bcd01ea68c031d
Enoz <enoz@enoz.dev>
2025-12-06 12:55:56 -0600

    Switch to codecompanion

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -9,31 +9,17 @@ return {
 		end,
 	},
 	{
-		"CopilotC-Nvim/CopilotChat.nvim",
+		"olimorris/codecompanion.nvim",
 		dependencies = {
-			{ "nvim-lua/plenary.nvim", branch = "master" },
+			"nvim-lua/plenary.nvim",
 		},
-		build = "make tiktoken",
 		config = function()
-			local chat = require("CopilotChat")
-			chat.setup({
-				temperature = 0.1, -- Lower = focused, higher = creative
-				window = {
-					layout = "vertical",
-					width = 0.45,
-				},
-				auto_insert_mode = false,
+			local codecompanion = require("codecompanion")
+			codecompanion.setup({
+				ignore_warnings = true,
 			})
-			vim.api.nvim_create_autocmd("BufEnter", {
-				pattern = "copilot-*",
-				callback = function()
-					vim.opt_local.relativenumber = false
-					vim.opt_local.number = false
-					vim.opt_local.conceallevel = 0
-				end,
-			})
-			vim.keymap.set({ "n", "v" }, "<leader>gc", chat.toggle, { desc = "CopilotChat Toggle" })
-			vim.keymap.set({ "n", "v" }, "<leader>gm", chat.select_model, { desc = "CopilotChat Toggle" })
+
+			vim.keymap.set({ "n", "v" }, "<leader>gc", codecompanion.chat, { desc = "AI Chat" })
 		end,
 	},
 }
