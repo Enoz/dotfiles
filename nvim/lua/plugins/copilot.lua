878ae2d6f0255566a983ad44c66040d71ab8f544
Enoz <enoz@enoz.dev>
2025-12-05 19:17:52 -0600

    Add select_model bind

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -17,11 +17,10 @@ return {
 		config = function()
 			local chat = require("CopilotChat")
 			chat.setup({
-				model = "gpt-4.1", -- AI model to use
 				temperature = 0.1, -- Lower = focused, higher = creative
 				window = {
-					layout = "vertical", -- 'vertical', 'horizontal', 'float'
-					width = 0.4, -- 40% of screen width
+					layout = "vertical",
+					width = 0.45,
 				},
 				auto_insert_mode = false,
 			})
@@ -34,6 +33,7 @@ return {
 				end,
 			})
 			vim.keymap.set({ "n", "v" }, "<leader>gc", chat.toggle, { desc = "CopilotChat Toggle" })
+			vim.keymap.set({ "n", "v" }, "<leader>gm", chat.select_model, { desc = "CopilotChat Toggle" })
 		end,
 	},
 }
