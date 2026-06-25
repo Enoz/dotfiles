c773846c427de4fab25637fc9c6f23fc0367c6d4
Enoz <enoz@enoz.dev>
2026-06-25 15:08:35 -0500

    feat: more config on codediff

--- nvim/plugin/codediff.lua
+++ nvim/plugin/codediff.lua
@@ -9,13 +9,11 @@ require("codediff").setup({
 	keymaps = {
 		view = {
 			quit = "q",
-			close_on_open_in_prev_tab = true,
 		},
 	},
 })
 
 vim.keymap.set("n", "<leader>gv", ":CodeDiff<CR>", { desc = "Open CodeDiff explorer" })
-vim.keymap.set("n", "<leader>gV", ":tabclose<CR>", { desc = "Close CodeDiff tab" })
 vim.keymap.set("n", "<leader>gf", ":CodeDiff history %<CR>", { desc = "File history" })
 vim.keymap.set("n", "<leader>gd", function()
 	vim.ui.input({ prompt = "Branch to diff against: ", default = "main" }, function(branch)
