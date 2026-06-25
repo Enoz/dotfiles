ad589a30808c15b4bac3adca79f559d8b7d81d21
Enoz <enoz@enoz.dev>
2026-06-25 09:44:45 -0500

    feat: add branch diff

--- nvim/plugin/diffview.lua
+++ nvim/plugin/diffview.lua
@@ -7,3 +7,10 @@ require("diffview").setup({})
 vim.keymap.set("n", "<leader>gv", ":DiffviewOpen<CR>", { desc = "Open DiffView" })
 vim.keymap.set("n", "<leader>gV", ":DiffviewClose<CR>", { desc = "Close DiffView" })
 vim.keymap.set("n", "<leader>gf", ":DiffviewFileHistory %<CR>", { desc = "File history" })
+vim.keymap.set("n", "<leader>gd", function()
+	vim.ui.input({ prompt = "Branch to diff against: ", default = "origin/main" }, function(branch)
+		if branch and #branch > 0 then
+			vim.cmd("DiffviewOpen " .. branch .. "...HEAD")
+		end
+	end)
+end, { desc = "Diff branch vs base" })
