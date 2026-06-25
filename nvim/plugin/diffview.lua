bed602c5e13b94b4858d4ec33b72e913133397f8
Enoz <enoz@enoz.dev>
2026-06-25 15:06:06 -0500

    feat: fix branch default

--- nvim/plugin/diffview.lua
+++ nvim/plugin/diffview.lua
@@ -15,9 +15,9 @@ vim.keymap.set("n", "<leader>gv", ":CodeDiff<CR>", { desc = "Open CodeDiff explo
 vim.keymap.set("n", "<leader>gV", ":tabclose<CR>", { desc = "Close CodeDiff tab" })
 vim.keymap.set("n", "<leader>gf", ":CodeDiff history %<CR>", { desc = "File history" })
 vim.keymap.set("n", "<leader>gd", function()
-	vim.ui.input({ prompt = "Branch to diff against: ", default = "origin/main" }, function(branch)
+	vim.ui.input({ prompt = "Branch to diff against: ", default = "main" }, function(branch)
 		if branch and #branch > 0 then
-			vim.cmd("CodeDiff " .. branch)
+			vim.cmd("CodeDiff " .. branch .. "...HEAD")
 		end
 	end)
 end, { desc = "Diff branch vs HEAD" })
